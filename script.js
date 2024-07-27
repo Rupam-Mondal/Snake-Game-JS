bc87d565dc0ac1fd1f295b0c9fccef585a96babb
Rupam <rupammondal564@gmail.com>
2024-07-27 22:17:41 +0530

    UI updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log(board)
 let snake = [{ x: 12, y: 16 }]
 let food = { x: 8, y: 9 }
 const foodCount = document.getElementById('score')
-let value = Number(foodCount.innerHTML)
+// let value = Number(foodCount.innerHTML)
 let direction = 'right'
 let gameInterval
 let gamestarted = false
@@ -13,18 +13,19 @@ let speed = 400
 function renderElements() {
     board.innerHTML = ""
     //snake render
-    snake.forEach((cell) => {
+    snake.forEach((cell , index) => {
         const celldiv = document.createElement('div')
-        celldiv.style.backgroundImage = 'url(Pictures/snake.png)'
-        celldiv.style.backgroundSize = 'cover'
         celldiv.style.gridColumnStart = cell.x
         celldiv.style.gridRowStart = cell.y
-        celldiv.classList.add('snake')
+        if(index == 0){
+            celldiv.classList.add('head')
+        }
+        else{
+            celldiv.classList.add('snake')
+        }
         board.appendChild(celldiv)
     })
     const fooddiv = document.createElement('div')
-    fooddiv.style.backgroundImage = 'url(Pictures/apple.png)'
-    fooddiv.style.backgroundSize = 'cover'
     fooddiv.style.gridColumnStart = food.x
     fooddiv.style.gridRowStart = food.y
     fooddiv.classList.add('food')
@@ -54,8 +55,8 @@ function move() {
     snake.unshift(head)
     // food eating logic
     if (head.x == food.x && head.y == food.y) {
-        food.x = Math.floor(Math.random() * 20) + 1
-        food.y = Math.floor(Math.random() * 20) + 1
+        food.x = Math.floor(Math.random() * 18) + 1
+        food.y = Math.floor(Math.random() * 18) + 1
         speed = speed - 20
         clearInterval(gameInterval)
         gameInterval = setInterval(() => {
@@ -63,9 +64,9 @@ function move() {
             checkCollision()
             renderElements()
         }, speed);
-        value += 1;
-        console.log(value);
-        foodCount.innerHTML = `${value}`
+        // value += 1;
+        // console.log(value);
+        // foodCount.innerHTML = `${value}`
     }
     else {
         snake.pop()
@@ -87,7 +88,7 @@ function startGame() {
 function checkCollision() {
     let head = { ...snake[0] }
 
-    if (head.x < 1 || head.x > 20 || head.y < 1 || head.y > 20) {
+    if (head.x < 1 || head.x > 18 || head.y < 1 || head.y > 18) {
         clearInterval(gameInterval);
         gamestarted = false;
         alert('Game Over! Press any key to restart.');
